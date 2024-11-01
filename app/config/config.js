bd34421d2db288e21fe176dd0fcae94c77c5f1dd
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-11-01 09:10:14 +0600

    config.js old file copy

---
+++ app/config/config.js
@@ -0,0 +1,26 @@
+import * as path from "node:path";
+
+export const MONGODB_CONNECTION="mongodb+srv://mrrabbil:mernx123@cluster0.rtpbcy6.mongodb.net/app_todo?retryWrites=true&w=majority";
+
+export const JWT_SECRET = "5EC7CEFA1BE7C9354A639369A2AA8JS";
+export const JWT_EXPIRATION_TIME = 60*60*24*30;
+
+export const EMAIL_HOST = "";
+export const EMAIL_PORT = "";
+export const EMAIL_USER = "";
+export const EMAIL_PASSWORD = "";
+
+export const Max_JSON_SIZE = "50mb";
+export const URL_ENCODER = true;
+
+
+export const REQUEST_LIMIT_TIME = 15*60*1000;//15 min
+export const REQUEST_LIMIT_NUMBER =3000;//per 15 Min 3000 Requests Allowed
+
+
+export const WEB_CACHE=false;
+export const PORT=5050;
+
+export function UPLOAD_FOLDER(fileName){
+    return path.resolve(process.cwd(), 'storage', fileName);
+}
